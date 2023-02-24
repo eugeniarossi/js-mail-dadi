4ea3d57881e52f4070715c21654ce02b8018f15e
eugeniarossi <eugenia.rossi@outlook.com>
2023-02-24 15:28:23 +0100

    feat aggiornamento esercizio dadi

--- dadi/js/main.js
+++ dadi/js/main.js
@@ -1,16 +1,29 @@
 'use strict';
 
-// Definisco due variabili, giocatore e computer, con numero random da 1 a 6
-const player = Math.floor(Math.random() * 5) +1;
-console.log({player})
-const computer = Math.floor(Math.random() * 5) +1;
-console.log({computer})
+// Definisco una variabile in cui salvo il play button
+const playButton = document.getElementById('play-button');
+// Definisco una variabile in cui salvo il div in cui mostrerò il risultato
+let risultato = document.getElementById('risultato');
 
-// Stabilisco delle condizioni in cui confronto le due variabili
-if (player > computer) {
-    console.log('Hai vinto');
-} else if (player === computer) {
-    console.log('Pari');
-} else {
-    console.log('Hai perso')
-}
+// Aggiungo un evento onclick al play button per far partire il gioco
+playButton.addEventListener('click', 
+    function () {
+        // Definisco due variabili, giocatore e computer, con numero random da 1 a 6
+        const player = Math.floor(Math.random() * 5) +1;
+        console.log({player});
+        const computer = Math.floor(Math.random() * 5) +1;
+        console.log({computer});
+
+        // Stabilisco delle condizioni in cui confronto le due variabili
+        if (player > computer) {
+            console.log('Hai vinto');
+            risultato.innerText = ('Hai vinto');
+        } else if (player === computer) {
+            console.log('Pari');
+            risultato.innerText = ('Pari');
+        } else {
+            console.log('Hai perso');
+            risultato.innerText = ('Hai perso');
+        }
+});
+
